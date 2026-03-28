c5b6b86ad3e82c7eecea2b8bd328a359b17965f2
RXRD <lilixinxin@protonmail.com>
2026-03-28 18:24:28 +0700

    chore: update subnet cidr

--- terraform/network/aws.tf
+++ terraform/network/aws.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "main_aws_vpc" {
 
 resource "aws_subnet" "aws_kubernetes_node_subnet" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.57.1.0/27"
+  cidr_block = "10.57.1.0/24"
 
   tags = {
     role = "kubernetes"
